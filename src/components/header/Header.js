9b75a7549584b0c77cd4b8829cff6d537ac29ed5
Ken Charette <charettekenneth@gmail.com>
2020-02-26 13:00:34 -0500

    Added to the user-sagas a signOutSuccess and signOutFailure user action process

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,13 +5,13 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { auth } from "../../firebase/firebase.utils";
 import { selectCartHidden } from "../../redux/cart/cart-selectors";
 import { selectCurrentUser } from "../../redux/user/user-selectors";
+import { signOutStart } from "../../redux/user/user-actions";
 
 import "./header-styles.scss";
 
-const Header = ({ currentUser, hidden }) => {
+const Header = ({ currentUser, hidden, signOutStart }) => {
 	return (
 		<div className="header">
 			<Link className="logo-container" to="/">
@@ -25,7 +25,7 @@ const Header = ({ currentUser, hidden }) => {
 					CONTACT
 				</Link>
 				{currentUser ? (
-					<div className="option" onClick={() => auth.signOut()}>
+					<div className="option" onClick={signOutStart}>
 						{" "}
 						SIGN OUT{" "}
 					</div>
@@ -52,4 +52,8 @@ const mapStateToProps = createStructuredSelector({
 	hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+	signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
